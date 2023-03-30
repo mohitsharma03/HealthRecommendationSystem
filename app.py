1453ec635fd127c8eaae1d52958bd65f29e5c551
mohitsharma03 <03mohitsharma@gmail.com>
2023-03-31 04:38:28 +0530

    mongodb code

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import csv
 from flask import Flask, jsonify, render_template, request, Response, redirect, make_response
 from pyspark.ml.regression import DecisionTreeRegressor
 from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions
 from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.types import StructType, StructField, IntegerType, DoubleType
+from pyspark.sql.functions import col
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def login():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
@@ -26,16 +30,77 @@ def predict():
         file.save(temp_file.name)
         temp_file.close()
 
+        '''
+        # Get the raw data
+        with open(temp_file.name, 'r') as f:
+            lines = csv.reader(f)
+        # Convert it to a RDD of Row objects with (userID, age, gender, occupation, zip)
+        users = lines.map(parseInput)
+        # Convert that to a DataFrame
+        usersDataset = spark.createDataFrame(users)
+        '''
+
+        # Define the schema of the CSV file
+        schema = StructType([
+            StructField("Pregnancies", IntegerType(), True),
+            StructField("Glucose", DoubleType(), True),
+            StructField("BloodPressure", DoubleType(), True),
+            StructField("SkinThickness", DoubleType(), True),
+            StructField("Insulin", DoubleType(), True),
+            StructField("BMI", DoubleType(), True),
+            StructField("DiabetesPedigreeFunction", DoubleType(), True),
+            StructField("Age", IntegerType(), True),
+            StructField("diabetes", IntegerType(), True)
+        ])
+
+        # Read the CSV file using the specified schema
+        usersDataset = spark.read.format("csv")\
+            .option("header", "true")\
+            .schema(schema)\
+            .load(temp_file.name)
+        
+        # Write it into MongoDB
+        usersDataset.write\
+            .format("com.mongodb.spark.sql.DefaultSource")\
+            .option("uri","mongodb://127.0.0.1/mydb.users")\
+            .mode('append')\
+            .save()
+
+        # Read it back from MongoDB into a new Dataframe
+        data = spark.read\
+        .format("com.mongodb.spark.sql.DefaultSource")\
+        .option("uri","mongodb://127.0.0.1/mydb.users")\
+        .load()
+
+        # Cast columns to the appropriate data type
+        data = data.select(
+            col("Pregnancies").cast(IntegerType()),
+            col("Glucose").cast(DoubleType()),
+            col("BloodPressure").cast(DoubleType()),
+            col("SkinThickness").cast(DoubleType()),
+            col("Insulin").cast(DoubleType()),
+            col("BMI").cast(DoubleType()),
+            col("DiabetesPedigreeFunction").cast(DoubleType()),
+            col("Age").cast(IntegerType()),
+            col("diabetes").cast(IntegerType())
+        )
+
+        #data.createOrReplaceTempView("users")
+        
+        #sqlDF = spark.sql("SELECT * FROM users WHERE age < 20")
+        #sqlDF.show()
+
         # Load data as a dataframe
-        data = spark.read.option("header", "true").option("inferSchema", "true").csv(temp_file.name)
+        #data = spark.read.option("header", "true").option("inferSchema", "true").csv(temp_file.name)
 
         # Assemble features into a vector column
         assembler = VectorAssembler().setInputCols(["Pregnancies","Glucose","BloodPressure",\
                                                     "SkinThickness","Insulin","BMI",\
                                                     "DiabetesPedigreeFunction","Age",]).\
                                                     setOutputCol("features")
+                                                    
         df = assembler.transform(data).select("diabetes", "features")
-
+        
         # Split data into training and testing data
         trainTest = df.randomSplit([0.8, 0.2])
         trainingDF = trainTest[0]
@@ -51,6 +116,7 @@ def predict():
         labels = fullPredictions.select("diabetes").rdd.map(lambda x: x[0])
         predictionAndLabel = predictions.zip(labels).collect()
 
+        
         # Write predictions to in-memory CSV file
         csv_file = StringIO()
         predictions_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -66,6 +132,6 @@ def predict():
 
         # Render the predictions in an HTML table
         return render_template('predictions.html', data=data)
-
+        
 if __name__ == '__main__':
     app.run(debug=True)
